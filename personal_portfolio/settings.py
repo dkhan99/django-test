7a8e80595c30cd09ffa19a0d76b4c53d85a934c2
Danyal Khan <danyalkhan11@gmail.com>
2021-07-14 22:40:26 -0400

    adding allowed host to all

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -129,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
