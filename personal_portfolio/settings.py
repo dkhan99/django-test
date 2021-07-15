961f48301d4bfe3dd3e87638a40e098004e32043
Danyal Khan <danyalkhan11@gmail.com>
2021-07-14 22:00:46 -0400

    add eniroment variable and gitignore

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-74*-aht4ec@2if@-i0^-793mh(mcg^5bhg69mqe_g#(hem@5-#'
+# SECRET_KEY = 'django-insecure-74*-aht4ec@2if@-i0^-793mh(mcg^5bhg69mqe_g#(hem@5-#'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure-74*-aht4ec@2if@-i0^-793mh(mcg^5bhg69mqe_g#(hem@5-#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
